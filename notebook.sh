33d5e20b44ef91db3afedd54f7a923192dab83b8
Leo Reiter <leo.reiter@nimbix.net>
2019-04-03 13:17:08 -0500

    force bash for terminal shell

--- notebook.sh
+++ notebook.sh
@@ -9,6 +9,7 @@ redir --laddr=0.0.0.0 --lport=443 --caddr=127.0.0.1 --cport=8888 &
 
 cd /usr/local/notebook
 sudo chown -R `id -u`:`id -g` /usr/local/notebook/samples &
+export SHELL=/bin/bash
 exec jupyter notebook --ip=127.0.0.1 --no-browser --port=8888 \
     --certfile=/etc/JARVICE/cert.pem \
     --NotebookApp.allow_remote_access=True \
