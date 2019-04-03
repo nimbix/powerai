d96b7546f7c8539a293e275378bf9b9338d09906
Leo Reiter <leo.reiter@nimbix.net>
2019-04-03 12:52:07 -0500

    use sudo to change ownership of samples

--- notebook.sh
+++ notebook.sh
@@ -8,7 +8,7 @@ conda activate
 redir --laddr=0.0.0.0 --lport=443 --caddr=127.0.0.1 --cport=8888 &
 
 cd /usr/local/data
-chown -R `id -u`:`id -g` /usr/local/samples
+sudo chown -R `id -u`:`id -g` /usr/local/samples
 exec jupyter notebook --ip=127.0.0.1 --no-browser --port=8888 \
     --certfile=/etc/JARVICE/cert.pem \
     --NotebookApp.allow_remote_access=True \
